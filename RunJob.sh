194b5ad5690a019055bc2f955f8d175e5e635159
Zhun-Yong Ong <ongzy@ihpc.a-star.edu.sg>
2024-10-15 10:17:21 +0800

    Updated example RunJob.sh file showing how the XAGF code should be run.

--- RunJob.sh
+++ RunJob.sh
@@ -1,40 +1,15 @@
 #!/bin/bash
 
 CURRDIR=$(pwd)
-AGFDIR=${CURRDIR}
-INPDIR=${CURRDIR}/input_files_cnt
-OUTDIR=${CURRDIR}/output_files_cnt
-# AGFDIR=/home/ongzy/IHPC_Documents/ihpc_research/2017-12-12_FullSMatrix/SourceCode
-# INPDIR=/home/ongzy/IHPC_Documents/ihpc_research/2018-08-29_CNTJunction/input_files
-# OUTDIR=/home/ongzy/IHPC_Documents/ihpc_research/2018-08-29_CNTJunction/output_files
+AGFDIR=${CURRDIR} # working directory containing XAGF code
+INPDIR=${CURRDIR}/input_files_cnt  # directory containing input files for AGF calculations
+OUTDIR=${CURRDIR}/output_files_cnt # directory containing output files from AGF calculations
 
 cd ${AGFDIR}
 matlab -nodisplay -nodesktop -r "ExtendedAtomisticGreensFunctionTransmission('${INPDIR}','${OUTDIR}'); exit" 
 cd ${CURRDIR}
 
-# ===================================================================================
-exit 1
-
-
-# ===================================================================================
-
-cp ${CURRDIR}/IMJ_4_pairs/input_files/{Left,Center,Right}_*.agf ${CURRDIR}/input_files_lowfreq/
-
-cd ${AGFDIR}
-matlab -nodisplay -nodesktop -r "ExtendedAtomisticGreensFunctionTransmission('${INPDIR}','${OUTDIR}'); exit" 
-cd ${CURRDIR}
-
-cp ${CURRDIR}/output_files_lowfreq/*.dat ${CURRDIR}/IMJ_4_pairs/output_files_lowfreq/
-
-# ===================================================================================
 exit 1
 
 # ===================================================================================
 
-cp ${CURRDIR}/IMJ_8_pairs/input_files/{Left,Center,Right}_*.agf ${CURRDIR}/input_files_lowfreq/
-
-cd ${AGFDIR}
-matlab -nodisplay -nodesktop -r "ExtendedAtomisticGreensFunctionTransmission('${INPDIR}','${OUTDIR}'); exit" 
-cd ${CURRDIR}
-
-cp ${CURRDIR}/output_files_lowfreq/*.dat ${CURRDIR}/IMJ_8_pairs/output_files_lowfreq/
